65f5fe9c04d8d79faa6ddf15fc58c3baf10ca461
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2022-08-10 10:22:25 -0700

    [ORO-0] Regenerate fatbin and fix script

--- UnitTest/bitcodes/generate_bitcodes_nvidia.sh
+++ UnitTest/bitcodes/generate_bitcodes_nvidia.sh
@@ -1,4 +1,4 @@
-nvcc -arch=compute_80 -code="sm_80,sm_86,sm_87" -fatbin --device-c ..\moduleTestFunc.cu
-nvcc -arch=compute_80 -code="sm_80,sm_86,sm_87" -fatbin --device-c ..\moduleTestKernel.cu
-nvcc -cubin --device-c -arch=sm_80 ..\moduleTestFunc.cu
-nvcc -cubin --device-c -arch=sm_80 ..\moduleTestKernel.cu
+nvcc -arch=compute_80 -code="sm_80,sm_86,sm_87" -fatbin --device-c ../moduleTestFunc.cu
+nvcc -arch=compute_80 -code="sm_80,sm_86,sm_87" -fatbin --device-c ../moduleTestKernel.cu
+nvcc -cubin --device-c -arch=sm_80 ../moduleTestFunc.cu
+nvcc -cubin --device-c -arch=sm_80 ../moduleTestKernel.cu
