5aa9921fe3ffeebc39f154dcb6edff48820b5975
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2022-08-14 17:28:31 +0900

    [ORO-0] Add unit test for orortcLinkAddFile

--- UnitTest/main.cpp
+++ UnitTest/main.cpp
@@ -241,6 +241,81 @@ TEST_F( OroTestBase, link )
 	}
 }
 
+TEST_F( OroTestBase, link_addFile )
+{
+	oroDeviceProp props;
+	OROCHECK( oroGetDeviceProperties( &props, m_device ) );
+	const bool isAmd = oroGetCurAPI( 0 ) == ORO_API_HIP;
+	std::string archName( props.gcnArchName );
+	archName = archName.substr( 0, archName.find( ':' ) );
+
+	std::string bcFile1 = isAmd ? ( "../UnitTest/bitcodes/moduleTestFunc-hip-amdgcn-amd-amdhsa-" + archName + ".bc" )
+		: "../UnitTest/bitcodes/moduleTestFunc.cubin";
+	std::string bcFile2 = isAmd ? ( "../UnitTest/bitcodes/moduleTestKernel-hip-amdgcn-amd-amdhsa-" + archName + ".bc" )
+		: "../UnitTest/bitcodes/moduleTestKernel.cubin";
+
+	{
+		orortcLinkState rtc_link_state;
+		orortcJIT_option options[6];
+		void* option_vals[6];
+		float wall_time;
+		unsigned int log_size = 8192;
+		char error_log[8192];
+		char info_log[8192];
+		size_t out_size;
+		void* cuOut;
+
+		options[0] = ORORTC_JIT_WALL_TIME;
+		option_vals[0] = (void*)( &wall_time );
+
+		options[1] = ORORTC_JIT_INFO_LOG_BUFFER;
+		option_vals[1] = info_log;
+
+		options[2] = ORORTC_JIT_INFO_LOG_BUFFER_SIZE_BYTES;
+		option_vals[2] = (void*)( log_size );
+
+		options[3] = ORORTC_JIT_ERROR_LOG_BUFFER;
+		option_vals[3] = error_log;
+
+		options[4] = ORORTC_JIT_ERROR_LOG_BUFFER_SIZE_BYTES;
+		option_vals[4] = (void*)( log_size );//todo. behavior difference
+
+		options[5] = ORORTC_JIT_LOG_VERBOSE;
+		option_vals[5] = (void*)1;
+
+		void* binary;
+		size_t binarySize;
+
+		orortcJITInputType type = isAmd ? ORORTC_JIT_INPUT_LLVM_BITCODE : ORORTC_JIT_INPUT_CUBIN;
+
+		ORORTCCHECK( orortcLinkCreate( 6, options, option_vals, &rtc_link_state ) );
+
+		ORORTCCHECK( orortcLinkAddFile( rtc_link_state, type, bcFile1.c_str(), 0, 0, 0 ) );
+		ORORTCCHECK( orortcLinkAddFile( rtc_link_state, type, bcFile2.c_str(), 0, 0, 0 ) );
+		// getting HIPRTC_ERROR_BUILTIN_OPERATION_FAILURE here
+		ORORTCCHECK( orortcLinkComplete( rtc_link_state, &binary, &binarySize ) );
+
+		oroFunction function;
+		oroModule module;
+		oroError ee = oroModuleLoadData( &module, binary );
+		OrochiUtils::waitForCompletion();
+		ee = oroModuleGetFunction( &function, module, "testKernel" );
+		int x_host = -1;
+		int* x_device = nullptr;
+		OROCHECK( oroMalloc( (oroDeviceptr*)&x_device, sizeof( int ) ) );
+		OROCHECK( oroMemset( (oroDeviceptr)x_device, 0, sizeof( int ) ) );
+		const void* args[] = { &x_device };
+
+		OrochiUtils::launch1D( function, 64, args, 64 );
+		OrochiUtils::waitForCompletion();
+		OROCHECK( oroMemcpyDtoH( &x_host, (oroDeviceptr)x_device, sizeof( int ) ) );
+		OROASSERT( x_host == 2016 );
+		OROCHECK( oroFree( (oroDeviceptr)x_device ) );
+		ORORTCCHECK( orortcLinkDestroy( rtc_link_state ) );
+		ORORTCCHECK( oroModuleUnload( module ) );
+	}
+}
+
 TEST_F( OroTestBase, link_null_name ) 
 {
 	oroDeviceProp props;
