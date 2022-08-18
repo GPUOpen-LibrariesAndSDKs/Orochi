d652e8e836d1cbf8841578dc7652a3d1bb779110
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2022-08-19 00:55:19 +0900

    [ORO-0] Add bitcode+bundled bitcode link test

--- scripts/unittest.bat
+++ scripts/unittest.bat
@@ -1,1 +1,1 @@
-..\dist\bin\release\Unittest64.exe --gtest_filter=-*link_addFile*:*link_bundledBc*:*getErrorString* --gtest_output=xml:../result.xml
+..\dist\bin\release\Unittest64.exe --gtest_filter=-*link_addFile*:*link_bundledBc*:*link_bundledBc_with_bc*:*getErrorString* --gtest_output=xml:../result.xml
