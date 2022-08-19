979e0c55dc9ddf4d942fdbe14a7dd4656140104b
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2022-08-19 17:29:12 +0900

    [ORO-0] Fix typo in script

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -1,1 +1,1 @@
-../dist/bin/release/Unittest64 --gtest_filter=-*link_addFile*:*link_bundledBc*:*link_bundledBc_with_bc*:*getErrorString* --gtest_output=xml:../result.xml
+../dist/bin/Release/Unittest64 --gtest_filter=-*link_addFile*:*link_bundledBc*:*link_bundledBc_with_bc*:*getErrorString* --gtest_output=xml:../result.xml
