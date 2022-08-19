82a6fa6a9f0523923bfc91455df3f775b746cb7f
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2022-08-19 17:42:25 +0900

    [ORO-0] Update linux TC script

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -1,1 +1,1 @@
-../dist/bin/Release/Unittest64 --gtest_filter=-*link_addFile*:*link_bundledBc*:*link_bundledBc_with_bc*:*getErrorString* --gtest_output=xml:../result.xml
+../dist/bin/Release/Unittest64 --gtest_filter=-*link*:*getErrorString* --gtest_output=xml:../result.xml
