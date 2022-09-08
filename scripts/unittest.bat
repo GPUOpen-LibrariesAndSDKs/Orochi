ce8079711523c26b9f7055c84febd903cccf9f81
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2022-09-08 22:03:00 +0900

    [ORO-0] Update unittest filter

--- scripts/unittest.bat
+++ scripts/unittest.bat
@@ -1,1 +1,1 @@
-..\dist\bin\Release\Unittest64.exe --gtest_filter=-*getErrorString* --gtest_output=xml:../result.xml
+..\dist\bin\Release\Unittest64.exe --gtest_filter=-*getErrorString*:*link_bundledBc_with_bc_loweredName* --gtest_output=xml:../result.xml
