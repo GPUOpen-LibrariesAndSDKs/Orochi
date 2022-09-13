f401a29869bc4eae64b0d10f3ae35e284cb42bf7
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2022-09-14 01:02:13 +0900

    [ORO-0] Update HIP dll's for bitcode+bundled bitcode linking support (#46)
    * [ORO-0] Update HIP dll's for bitcode linking support
    
    * [ORO-0] Add getLoweredName testcase
    
    * [ORO-0] Update unittest filter
    
    * [ORO-0] Update loweredName test
    
    * [ORO-0] Add missing test kernel
    
    * [ORO-0] Fix loweredName test
    
    * [ORO-0] Fix linux compilation

--- UnitTest/main.cpp
+++ UnitTest/main.cpp
@@ -532,6 +532,99 @@ TEST_F( OroTestBase, link_bundledBc_with_bc )
 	}
 }
 
+TEST_F( OroTestBase, link_bundledBc_with_bc_loweredName )
+{
+	oroDeviceProp props;
+	OROCHECK( oroGetDeviceProperties( &props, m_device ) );
+	int v;
+	oroDriverGetVersion( &v );
+	std::vector<char> data0;
+	std::vector<char> data1;
+	const bool isAmd = oroGetCurAPI( 0 ) == ORO_API_HIP;
+	const char* funcName = "testKernel<0>";
+	std::string loweredNameStr;
+	orortcProgram prog;
+
+	{
+		std::string bcFile = isAmd ? "../UnitTest/bitcodes/moduleTestFunc-hip-amdgcn-amd-amdhsa.bc" : "../UnitTest/bitcodes/moduleTestFunc.fatbin";
+		loadFile( bcFile.c_str(), data1 );
+	}
+	{
+		std::vector<const char*> opts = isAmd ? std::vector<const char*>( { "-fgpu-rdc", "-c", "--cuda-device-only" } ) : std::vector<const char*>( { "--device-c", "-arch=sm_80" } );
+		std::string code;
+
+		OrochiUtils::readSourceCode( "../UnitTest/moduleTestKernel_loweredName.h", code );
+		OrochiUtils::getProgram( m_device, code.c_str(), "../UnitTest/moduleTestKernel_loweredName.h", &opts, funcName, &prog );
+		const char* loweredName = 0;
+		ORORTCCHECK( orortcGetLoweredName( prog, funcName, &loweredName ) );
+		loweredNameStr = std::string( loweredName );
+		size_t codeSize;
+		ORORTCCHECK( orortcGetBitcodeSize( prog, &codeSize ) );
+
+		data0.resize( codeSize );
+		ORORTCCHECK( orortcGetBitcode( prog, data0.data() ) );
+		ORORTCCHECK( orortcDestroyProgram( &prog ) );
+	}
+
+	{
+		orortcLinkState rtc_link_state;
+		orortcJIT_option options[7];
+		void* option_vals[7];
+		float wall_time;
+
+		unsigned int log_size = 8192;
+		char error_log[8192];
+		char info_log[8192];
+		size_t out_size;
+		void* cuOut;
+
+		options[0] = ORORTC_JIT_WALL_TIME;
+		option_vals[0] = (void*)( &wall_time );
+
+		options[1] = ORORTC_JIT_INFO_LOG_BUFFER;
+		option_vals[1] = info_log;
+
+		options[2] = ORORTC_JIT_INFO_LOG_BUFFER_SIZE_BYTES;
+		option_vals[2] = (void*)( log_size );
+
+		options[3] = ORORTC_JIT_ERROR_LOG_BUFFER;
+		option_vals[3] = error_log;
+
+		options[4] = ORORTC_JIT_ERROR_LOG_BUFFER_SIZE_BYTES;
+		option_vals[4] = (void*)( log_size ); // todo. behavior difference
+
+		options[5] = ORORTC_JIT_LOG_VERBOSE;
+		option_vals[5] = (void*)1;
+
+		void* binary;
+		size_t binarySize = 0;
+		const orortcJITInputType type0 = isAmd ? ORORTC_JIT_INPUT_LLVM_BITCODE : ORORTC_JIT_INPUT_CUBIN;
+		const orortcJITInputType type1 = isAmd ? ORORTC_JIT_INPUT_LLVM_BUNDLED_BITCODE : ORORTC_JIT_INPUT_FATBINARY;
+		ORORTCCHECK( orortcLinkCreate( 6, options, option_vals, &rtc_link_state ) );
+		ORORTCCHECK( orortcLinkAddData( rtc_link_state, type1, data1.data(), data1.size(), 0, 0, 0, 0 ) );
+		ORORTCCHECK( orortcLinkAddData( rtc_link_state, type0, data0.data(), data0.size(), 0, 0, 0, 0 ) );
+		ORORTCCHECK( orortcLinkComplete( rtc_link_state, &binary, &binarySize ) );
+
+		oroFunction function;
+		oroModule module;
+		oroError ee = oroModuleLoadData( &module, binary );
+		oroError e =  oroModuleGetFunction( &function, module, loweredNameStr.c_str() );
+		int x_host = -1;
+		int* x_device = nullptr;
+		OROCHECK( oroMalloc( (oroDeviceptr*)&x_device, sizeof( int ) ) );
+		OROCHECK( oroMemset( (oroDeviceptr)x_device, 0, sizeof( int ) ) );
+		const void* args[] = { &x_device };
+
+		OrochiUtils::launch1D( function, 64, args, 64 );
+		OrochiUtils::waitForCompletion();
+		OROCHECK( oroMemcpyDtoH( &x_host, (oroDeviceptr)x_device, sizeof( int ) ) );
+		OROASSERT( x_host == 2016 );
+		OROCHECK( oroFree( (oroDeviceptr)x_device ) );
+		ORORTCCHECK( orortcLinkDestroy( rtc_link_state ) );
+		ORORTCCHECK( oroModuleUnload( module ) );
+	}
+}
+
 TEST_F( OroTestBase, getErrorString )
 {
 	oroError error = (oroError)1;
--- scripts/unittest.bat
+++ scripts/unittest.bat
@@ -1,1 +1,1 @@
-..\dist\bin\Release\Unittest64.exe --gtest_filter=-*link_addFile*:*link_bundledBc*:*link_bundledBc_with_bc*:*getErrorString* --gtest_output=xml:../result.xml
+..\dist\bin\Release\Unittest64.exe --gtest_filter=-*getErrorString*:*link_bundledBc_with_bc_loweredName* --gtest_output=xml:../result.xml
