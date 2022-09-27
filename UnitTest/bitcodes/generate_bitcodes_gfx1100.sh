9a96dec9ac1b528fd34ea97a04aeb4fb86f401d2
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2022-09-27 09:26:28 +0900

    [ORO-0] Update test scripts (#50)

---
+++ UnitTest/bitcodes/generate_bitcodes_gfx1100.sh
@@ -0,0 +1,6 @@
+hipcc --cuda-device-only --offload-arch=gfx1100 --offload-arch=gfx1030 --offload-arch=gfx1031 --offload-arch=gfx1032 --offload-arch=gfx1033 --offload-arch=gfx1034 --offload-arch=gfx1035 --offload-arch=gfx1036 --offload-arch=gfx1010 --offload-arch=gfx1011 --offload-arch=gfx1012 --offload-arch=gfx1013 --offload-arch=gfx900 --offload-arch=gfx906 -fgpu-rdc -c --gpu-bundle-output -c -emit-llvm ../moduleTestKernel.cpp
+
+hipcc --cuda-device-only --offload-arch=gfx1100 --offload-arch=gfx1030 --offload-arch=gfx1031 --offload-arch=gfx1032 --offload-arch=gfx1033 --offload-arch=gfx1034 --offload-arch=gfx1035 --offload-arch=gfx1036 --offload-arch=gfx1010 --offload-arch=gfx1011 --offload-arch=gfx1012 --offload-arch=gfx1013 --offload-arch=gfx900 --offload-arch=gfx906 -fgpu-rdc -c --gpu-bundle-output -c -emit-llvm ../moduleTestFunc.cpp
+
+hipcc --offload-arch=gfx1100 ../moduleTestFunc.cpp -c -fgpu-rdc --cuda-device-only
+hipcc --offload-arch=gfx1100 ../moduleTestKernel.cpp -c -fgpu-rdc --cuda-device-only
---
+++ scripts/unittest_navi1.sh
@@ -0,0 +1,1 @@
+../dist/bin/Release/Unittest64 --gtest_filter=-*link*:*getErrorString* --gtest_output=xml:../result.xml
