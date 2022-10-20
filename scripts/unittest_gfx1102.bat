437b656c542e8759a57972c4f5aee9937845c62f
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2022-10-20 23:36:46 +0900

    [ORO-0] Add new scripts (#52)
    * [ORO-0] Add new scripts
    
    * [ORO-0] Add execute permissions to scripts

---
+++ scripts/unittest_gfx1102.bat
@@ -0,0 +1,4 @@
+cd ..\UnitTest\bitcodes
+call generate_bitcodes_gfx1102.bat
+cd ..\..\scripts
+..\dist\bin\Release\Unittest64 --gtest_filter=-*getErrorString* --gtest_output=xml:../result.xml
--- scripts/unittest_gfx1102.sh
+++ scripts/unittest_gfx1102.sh
@@ -1,3 +1,3 @@
-cd ../UnitTest/bitcodes && ./generate_bitcodes_gfx1100.sh
+cd ../UnitTest/bitcodes && ./generate_bitcodes_gfx1102.sh
 cd ../../scripts
 ../dist/bin/Release/Unittest64 --gtest_filter=-*link*:*getErrorString* --gtest_output=xml:../result.xml
