65b6243bfcc164f48a65fdc062ada6b5fd813cbc
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2022-11-22 20:23:57 +0900

    [ORO-0] Update test scripts to delete cache before running

--- scripts/unittest_gfx1100.sh
+++ scripts/unittest_gfx1100.sh
@@ -1,3 +1,4 @@
+rm -rf cache
 cd ../UnitTest/bitcodes && ./generate_bitcodes_gfx1100.sh
 cd ../../scripts
 ../dist/bin/Release/Unittest64 --gtest_filter=-*link*:*getErrorString* --gtest_output=xml:../result.xml
