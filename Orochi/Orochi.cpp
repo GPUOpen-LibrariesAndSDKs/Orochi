ed7ddb97670e6b8a2303a35093afdde4a0acf470
Aaryaman Vasishta <aaryaman.vasishta@amd.com>
2023-01-12 06:16:17 +0900

    [ORO-0] Add vulkan/dx interop functions/structs/enums (#59)
    * [ORO-0] Add vulkan/dx interop functions/structs/enums
    
    * [ORO-0] Add uint reserved[16] field in oroExternalSemaphoreHandleDesc

--- Orochi/Orochi.cpp
+++ Orochi/Orochi.cpp
@@ -658,26 +658,55 @@ oroError OROAPI oroModuleOccupancyMaxPotentialBlockSize( int* minGridSize, int* 
 }
 
 //-------------------
-oroError OROAPI oroImportExternalMemory(oroExternalMemory_t* extMem_out, const oroExternalMemoryHandleDesc* memHandleDesc)
+oroError OROAPI oroImportExternalMemory(oroExternalMemory* extMem_out, const oroExternalMemoryHandleDesc* memHandleDesc)
 {
 	__ORO_FUNC1( ImportExternalMemory( (CUexternalMemory*)extMem_out, (const CUDA_EXTERNAL_MEMORY_HANDLE_DESC*)memHandleDesc ),
 		ImportExternalMemory( (hipExternalMemory_t*)extMem_out, (const hipExternalMemoryHandleDesc*)memHandleDesc ) );
 	return oroErrorUnknown;
 }
 //-------------------
-oroError OROAPI oroExternalMemoryGetMappedBuffer(void **devPtr, oroExternalMemory_t extMem, const oroExternalMemoryBufferDesc* bufferDesc)
+oroError OROAPI oroImportExternalSemaphore(oroExternalSemaphore* extSem_out, const oroExternalSemaphoreHandleDesc* semHandleDesc)
 {
-	__ORO_FUNC1( ExternalMemoryGetMappedBuffer( (CUdeviceptr*)devPtr, (CUexternalMemory)extMem, (const CUDA_EXTERNAL_MEMORY_BUFFER_DESC*)bufferDesc ),
-		ExternalMemoryGetMappedBuffer( devPtr, (hipExternalMemory_t)extMem, (const hipExternalMemoryBufferDesc*)bufferDesc ) );
+	__ORO_FUNC1(ImportExternalSemaphore((CUexternalSemaphore*)extSem_out, (const CUDA_EXTERNAL_SEMAPHORE_HANDLE_DESC*)semHandleDesc),
+		ImportExternalSemaphore((hipExternalSemaphore_t*)extSem_out, (const hipExternalSemaphoreHandleDesc*)semHandleDesc));
 	return oroErrorUnknown;
 }
 //-------------------
-oroError OROAPI oroDestroyExternalMemory(oroExternalMemory_t extMem)
+oroError OROAPI oroExternalMemoryGetMappedBuffer(oroDeviceptr* devPtr, oroExternalMemory extMem, const oroExternalMemoryBufferDesc* bufferDesc)
+{
+	__ORO_FUNC1( ExternalMemoryGetMappedBuffer( (CUdeviceptr*)devPtr, (CUexternalMemory)extMem, (const CUDA_EXTERNAL_MEMORY_BUFFER_DESC*)bufferDesc ),
+		ExternalMemoryGetMappedBuffer( (void**)devPtr, (hipExternalMemory_t)extMem, (const hipExternalMemoryBufferDesc*)bufferDesc ) );
+	return oroErrorUnknown;
+}
+//-------------------
+oroError OROAPI oroDestroyExternalMemory(oroExternalMemory extMem)
 {
 	__ORO_FUNC1( DestroyExternalMemory( (CUexternalMemory)extMem ),
 		DestroyExternalMemory( (hipExternalMemory_t)extMem ) );
 	return oroErrorUnknown;
 }
+//-------------------
+oroError OROAPI oroDestroyExternalSemaphore(oroExternalSemaphore extSem)
+{
+	__ORO_FUNC1(DestroyExternalSemaphore((CUexternalSemaphore)extSem),
+		DestroyExternalSemaphore((hipExternalSemaphore_t)extSem));
+	return oroErrorUnknown;
+}
+//-------------------
+oroError OROAPI oroWaitExternalSemaphoresAsync(const oroExternalSemaphore* extSemArray, const oroExternalSemaphoreWaitParams* paramsArray, unsigned int numExtSems, oroStream stream)
+{
+	__ORO_FUNC1(WaitExternalSemaphoresAsync((CUexternalSemaphore*)extSemArray, (const CUDA_EXTERNAL_SEMAPHORE_WAIT_PARAMS*)paramsArray, numExtSems, (CUstream)stream),
+		WaitExternalSemaphoresAsync((hipExternalSemaphore_t*)extSemArray, (const hipExternalSemaphoreWaitParams*)paramsArray, numExtSems, (hipStream_t)stream));
+	return oroErrorUnknown;
+}
+//-------------------
+oroError OROAPI oroSignalExternalSemaphoresAsync(const oroExternalSemaphore* extSemArray, const oroExternalSemaphoreSignalParams* paramsArray, unsigned int numExtSems, oroStream stream)
+{
+	__ORO_FUNC1(SignalExternalSemaphoresAsync((CUexternalSemaphore*)extSemArray, (const CUDA_EXTERNAL_SEMAPHORE_SIGNAL_PARAMS*)paramsArray, numExtSems, (CUstream)stream),
+		SignalExternalSemaphoresAsync((hipExternalSemaphore_t*)extSemArray, (const hipExternalSemaphoreSignalParams*)paramsArray, numExtSems, (hipStream_t)stream));
+	return oroErrorUnknown;
+}
+
 /* oroError OROAPI oroGetLastError(oroError oro_error)
 {
 	__ORO_FUNC2(GetLastError((cudaError_t)oro_error),
