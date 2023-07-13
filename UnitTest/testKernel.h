fee94993054367558922b3d69c7b93eec888036d
Kulkarni <pariku@amd.com>
2023-07-13 10:40:05 -0400

    [ORO-0] update kernel

--- UnitTest/testKernel.h
+++ UnitTest/testKernel.h
@@ -26,7 +26,7 @@ extern "C" __global__ void testFuncPointerKernel( int* __restrict__ a, FuncPoint
 extern "C" __global__ void streamData(float *ptr, const size_t n, 
                               float* output, const float val) 
 { 
-  size_t idx = threadIdx.x; 
+  size_t idx = blockIdx.x *blockDim.x + threadIdx.x; 
   float accum = 0.0f; 
 
   for(; idx < n; idx += 1) 
