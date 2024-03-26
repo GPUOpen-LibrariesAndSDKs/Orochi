858b2e3b928471d1ae9aad823f717710886183d4
Richard Geslot <richard.geslot@amd.com>
2024-03-26 17:34:45 +0100

    Unit Test - improve linux script

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -1,3 +1,5 @@
 rm -rf cache
-cd ../UnitTest/bitcodes && ./generate_bitcodes.sh && cd ../../scripts
+cd ../UnitTest/bitcodes
+./generate_bitcodes.sh
+cd ../../scripts
 ../dist/bin/Release/Unittest64 --gtest_filter=-*link_bundledBc* --gtest_output=xml:../result.xml
