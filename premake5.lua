bb1d13e7a8622d75dac9228fc446cc55490f5229
Richard Geslot <richard.geslot@amd.com>
2024-03-29 18:36:00 +0100

    premake fix

--- premake5.lua
+++ premake5.lua
@@ -21,7 +21,7 @@ newoption {
 
 function copydir(src_dir, dst_dir, filter, single_dst_dir)
 	if not os.isdir(src_dir) then
-		printError("'%s' is not an existing directory!", src_dir)
+		print("copydir FAILED: " .. src_dir .. " is not an existing directory!" )
 	end
 	filter = filter or "**"
 	src_dir = src_dir .. "/"
