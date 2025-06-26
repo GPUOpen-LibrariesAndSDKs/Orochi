09ff500cde5151fd966eae06e7deb90aea596455
Sho Ikeda <Sho.Ikeda@amd.com>
2025-06-26 16:14:51 +0900

    [ORO-0] Add hip7 pathes for Ubuntu.

--- contrib/hipew/src/hipew.cpp
+++ contrib/hipew/src/hipew.cpp
@@ -644,6 +644,10 @@ void hipewInit( int* resultDriver, int* resultRtc, uint32_t flags, const char** 
   const char *hip_paths[] = { 
 
       // we first try the specific versions
+      "/opt/rocm/hip/lib/libamdhip64.so.7",
+      "/opt/rocm/lib/libamdhip64.so.7", 
+      "libamdhip64.so.7",
+
       "/opt/rocm/hip/lib/libamdhip64.so.6",
       "/opt/rocm/lib/libamdhip64.so.6", 
       "libamdhip64.so.6",
@@ -662,6 +666,10 @@ void hipewInit( int* resultDriver, int* resultRtc, uint32_t flags, const char** 
   const char* hiprtc_paths[] = { 
 
       // we first try the specific versions
+      "/opt/rocm/hip/lib/libhiprtc.so.7",
+      "/opt/rocm/lib/libhiprtc.so.7", 
+      "libhiprtc.so.7",
+
       "/opt/rocm/hip/lib/libhiprtc.so.6",
       "/opt/rocm/lib/libhiprtc.so.6", 
       "libhiprtc.so.6",
