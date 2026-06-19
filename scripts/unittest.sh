aa0d713d719bf41b6026c9bdb339f70f108b0575
Sho Ikeda <Sho.Ikeda@amd.com>
2026-06-19 15:15:21 +0900

    [ORO-0] Skip checkCUEW from regular test.

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -2,4 +2,4 @@ rm -rf cache
 cd ../UnitTest/bitcodes
 ./generate_bitcodes.sh
 cd ../../scripts
-../dist/bin/Release/Unittest64 --gtest_filter=-*link_bundledBc* --gtest_output=xml:../result.xml
+../dist/bin/Release/Unittest64 --gtest_filter=-*checkCUEW*:*link_bundledBc* --gtest_output=xml:../result.xml
